3155721b0bd264be4f51e0622bcff5083b32572b
mohitsharma16 <mohitsharmaofficial16@gmail.com>
2025-06-29 01:28:21 +0530

    feat: add imageviewer for viewing selected images in the app

--- app/src/main/java/com/example/datefinder/presentation/components/ImagePreview.kt
+++ app/src/main/java/com/example/datefinder/presentation/components/ImagePreview.kt
@@ -1,2 +1,36 @@
 package com.example.datefinder.presentation.components
 
+import android.net.Uri
+import android.widget.ImageView
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.viewinterop.AndroidView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
+
+@Composable
+fun ImagePreview(imageUri: Uri, modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+
+    AndroidView(
+        modifier = modifier,
+        factory = { ctx ->
+            ImageView(ctx).apply {
+                scaleType = ImageView.ScaleType.CENTER_CROP
+            }
+        },
+        update = { imageView ->
+            // This block runs every time imageUri changes
+            Glide.with(context)
+                .load(imageUri)
+                .apply(
+                    RequestOptions()
+                        .diskCacheStrategy(DiskCacheStrategy.ALL)
+                        .centerCrop()
+                )
+                .into(imageView)
+        }
+    )
+}
