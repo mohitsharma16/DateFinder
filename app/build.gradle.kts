16fb5b5c1e46ad62196ad74c62a3486904ca3376
mohitsharma16 <mohitsharmaofficial16@gmail.com>
2025-06-29 21:50:50 +0530

    chore: add package for kotlin datetime

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation(libs.kotlinx.datetime)
 
     // Glide (for loading image URIs)
     implementation(libs.glide)
